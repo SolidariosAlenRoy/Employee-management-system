a6eba82de49e16c5026719a0ff3debf0dea6a43d
jumeyla <142509110+jumeyla@users.noreply.github.com>
2024-02-06 22:49:37 +0800

    update

--- addEmployee.php
+++ addEmployee.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Style/addteacher.css">
-    <title>Attendance</title>
+    <title>Add Employee</title>
 </head>
 <body>
     <header class="header">
@@ -14,27 +14,22 @@
         </nav>
     </header>
     <div class="card">
-        <h2>Add Teacher</h2>
-        <form method = "post" id="teacherForm" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <label for="teacherID">Teacher ID:</label>
+        <h2>Add Employee</h2>
+        <form method = "post" id="employeeForm" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <label for="employeeID">Teacher ID:</label>
             <input type="text" id="ID" name="ID" required>
 
-            <label for="teacherName">Teacher First Name:</label>
+            <label for="employeeName">Employee First Name:</label>
             <input type="text" id="First_Name" name="First_Name" required>
 
-            <label for="teacherName">Teacher Middle Name:</label>
+            <label for="EmployeeName">Employee Middle Name:</label>
             <input type="text" id="Middle_Name" name="Middle_Name" required>
 
-            <label for="teacherName">Teacher Last Name:</label>
+            <label for="employeeName">Employee Last Name:</label>
             <input type="text" id="Last_Name" name="Last_Name" required>
 
             <label for="Job_Description">Job Description:</label>
-            <select name="Job_Description" id="Job_Description">
-                 <option value="Full Time">Full Time</option>
-                 <option value="Part Time">Part Time</option>
-                 <option value="Faculty">Faculty</option>
-             </select>
-            <br>
+            
             <input type="hidden" name="action" value="update">
             <button type="submit" value="submit">Submit</button>
         </form>
